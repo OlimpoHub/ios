3b1b2ec92a256a9d0b5fa7eaf714e0370506735e
Morgana119 <145613786+Morgana119@users.noreply.github.com>
2025-11-11 19:05:07 -0600

    [ADD] Calendar Offline files

---
+++ elArca/elArca/Data/Models/CoreData/CDCalendarItem.swift
@@ -0,0 +1,25 @@
+//
+//  CDCalendarItem.swift
+//  elArca
+//
+//  Created by Fátima Figueroa on 11/11/25.
+//
+
+import Foundation
+import CoreData
+
+@objc(CDCalendarItem)
+public class CDCalendarItem: NSManagedObject {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CDCalendarItem> {
+        return NSFetchRequest<CDCalendarItem>(entityName: "CDCalendarItem")
+    }
+
+    @NSManaged public var idTaller: String
+    @NSManaged public var idCapacitacion: String
+    @NSManaged public var idUsuario: String
+    @NSManaged public var nombreTaller: String
+    @NSManaged public var fecha: Date
+    @NSManaged public var horaEntrada: String
+    @NSManaged public var horaSalida: String
+}
