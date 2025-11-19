a7ecce0b57a10d065ec2d34e626f278dd9a33ec6
CsVazquezz <149129755+CsVazquezz@users.noreply.github.com>
2025-11-19 13:32:46 -0600

    [FIX] update elArcaApp due to conflicts after merge

--- elArca/elArca/elArcaApp.swift
+++ elArca/elArca/elArcaApp.swift
@@ -1,3 +1,4 @@
+
 //
 //  elArcaApp.swift
 //  elArca
@@ -11,16 +12,7 @@ import FlowStacks
 @main
 struct elArcaApp: App {
     @StateObject private var deepLinkRouter = DeepLinkRouter()
-
-    var body: some Scene {
-        WindowGroup {
-            AppBackground {
-                CoordinatorView()
-                    .preferredColorScheme(.dark)
-                    .environmentObject(deepLinkRouter)
-                    .onOpenURL { url in
-                        deepLinkRouter.handle(url)
-                    }
+    
     // Used to change the views
     @StateObject var router = CoordinatorViewModel()
     
@@ -30,8 +22,13 @@ struct elArcaApp: App {
     var body: some Scene {
         WindowGroup {
             AppBackground {
-                CoordinatorView(userNav: $userNav, notificationType: $notif).preferredColorScheme(.dark)
+                CoordinatorView(userNav: $userNav, notificationType: $notif)
                     .environmentObject(router)
+                    .environmentObject(deepLinkRouter)
+                    .preferredColorScheme(.dark)
+                    .onOpenURL { url in
+                        deepLinkRouter.handle(url)
+                    }
             }
         }
     }
