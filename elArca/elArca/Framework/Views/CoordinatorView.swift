da10e6d8628fbe0fe6389411cbd5ed002e4b4695
frida <a01707168@tec.mx>
2025-11-29 21:48:12 -0600

    [FIX] coordinator view scroll bar

--- elArca/elArca/Framework/Views/CoordinatorView.swift
+++ elArca/elArca/Framework/Views/CoordinatorView.swift
@@ -12,43 +12,36 @@ struct CoordinatorView: View {
     @Binding var userNav: UserNav
     @Binding var notificationType: NotificationType
     
-    // Lets the screens change
     @EnvironmentObject var router: CoordinatorViewModel
     @StateObject private var attendanceVM = AttendanceViewModel()
     
     var body: some View {
         
-        VStack(spacing: 0) {
+        ZStack(alignment: .bottom) {   // 👈 CAMBIO MÁS IMPORTANTE
+            
+            // Todas las pantallas
             Group {
                 switch router.screen {
                 case .login:
                     LoginView()
-                
                 case .home:
                     HomeView()
-                    
                 case .notifications:
                     NotificationView(notificationType: notificationType)
-                    
                 case .workshop:
                     WorkshopView()
-                    
                 case .calendar:
                     CalendarView()
-                    
                 case .beneficiaries:
                     Beneficiary()
-                    
                 case .attendance:
                     VStack {
                         Spacer()
                         ReadQRView(viewModel: AttendanceViewModel())
                         Spacer()
                     }
-
                 case .configuration:
                     ConfigurationView()
-
                 default:
                     VStack {
                         Spacer()
@@ -61,11 +54,16 @@ struct CoordinatorView: View {
                     }
                 }
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color("Bg"))
             
+            // NAV BAR SI APLICA
             if router.screen.isNavbarViewable {
                 NavBar(userNav: userNav)
+                    .frame(height: 65)
             }
         }
+        // 👇👇 EL FIX QUE EVITA QUE EL NAVBAR SE MUEVA CON EL TECLADO
+        .ignoresSafeArea(.keyboard, edges: .bottom)
     }
 }
